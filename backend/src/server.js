a29055bc27bee88d3ec4ce33453883dfceb23c5f
Abinash Mohanty <mohantyabinash35@gmail.com>
2025-10-22 11:41:14 +0530

    some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,39 +7,32 @@ import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
 const __dirname = path.resolve();
 const allowedOrigins = [
-  "http://localhost:5173", 
-  "https://chat-app-git-main-abhiasmis-projects.vercel.app", 
-  "https://chat-app-sigma-ten.vercel.app"
+  "http://localhost:5173",
 ];
-
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin) return callback(null, true); 
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = "The CORS policy for this site does not allow access from the specified origin.";
-        return callback(new Error(msg), false);
+      if (!origin) return callback(null, true);
+
+      if (!allowedOrigins.includes(origin)) {
+        return callback(new Error(`CORS error: Origin ${origin} not allowed`), false);
       }
       return callback(null, true);
     },
-    credentials: true, 
+    credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
@@ -48,6 +41,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`✅ Server running on port ${PORT}`);
   connectDB();
 });
