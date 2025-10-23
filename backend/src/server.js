a048764381e57e61cca2eafa9eafff8c985e23c8
Abinash Mohanty <mohantyabinash35@gmail.com>
2025-10-23 17:25:51 +0530

    new url

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,7 @@ const __dirname = path.resolve();
 app.set("trust proxy", 1);
 const allowedOrigins = [
   "http://localhost:5173",
-  "chat-app-sigma-ten.vercel.app"
+  "chat-app-dvb8.vercel.app"
 ];
 app.use(
   cors({
