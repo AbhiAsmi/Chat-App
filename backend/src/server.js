c0ab204df9dc891a972dca0925798585a181d4a5
Abinash Mohanty <mohantyabinash35@gmail.com>
2025-10-23 16:44:26 +0530

    cors set up

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,6 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
@@ -12,16 +11,28 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
+app.set("trust proxy", 1);
 const allowedOrigins = [
-  "http://localhost:5173",  
-  "chat-app-sigma-ten.vercel.app"              
+  "http://localhost:5173",
+  "chat-app-sigma-ten.vercel.app"
 ];
 app.use(
   cors({
-    origin: allowedOrigins,
-    credentials: true, 
+    origin: function (origin, callback) {
+
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
@@ -29,11 +40,11 @@ app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
+
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
   connectDB();
